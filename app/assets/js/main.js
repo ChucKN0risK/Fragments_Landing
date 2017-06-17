0bc679119da0a6a1e8509bcd30d943ce4a1016ee
Antoine Moreaux <moreaux.antoinne@gmail.com>
2017-06-17 17:04:16 +0200

    add connection google form

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -26,5 +26,56 @@
 // document.querySelector('.el').nextElementSibling;
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.info('main.js Loaded');
+  loaded()
 });
+
+function validEmail(email) { // see:
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+// get all data in form and return object
+function getFormData() {
+  return{
+    lastname: document.getElementById('lastname').value,
+    email: document.getElementById('email').value,
+    firstname: document.getElementById('firstname').value,
+    company: document.getElementById('company').value
+  }
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+  var data = getFormData();         // get the values submitted in the form
+  // if( !validEmail(data.email) ) {   // if email is not valid show error
+  //   document.getElementById('email-invalid').style.display = 'block';
+  //   return false;
+  // } else {
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+      if(xhr.status === 200){
+        document.getElementById('gform').style.display = 'none';
+        document.getElementById('sentence-form-head').innerText = 'Voilà ! Inscription terminée.';
+        document.getElementById('sentence-form-content').innerText = 'Vous venez de rejoindre la file d\'attente,' +
+          ' nous vous tenons au courant dès que l\'alpha vous sera ouverte.';
+
+      }
+      return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    event.preventDefault();           // we are submitting via xhr below
+
+    xhr.send(encoded);
+  // }
+}
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
